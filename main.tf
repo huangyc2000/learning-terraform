aece1d767939bf59e10062148fe1f1a2a091b3ad
huangyc2000 <157409995+huangyc2000@users.noreply.github.com>
2025-04-10 17:12:30 -0700

    create new dev vpc

--- main.tf
+++ main.tf
@@ -14,8 +14,32 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
 data "aws_vpc" "default" {
     default = true
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -24,6 +48,8 @@ resource "aws_instance" "blog" {
 
   /* vpc_security_group_ids = [aws_security_group.blog.id] */
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -36,7 +62,8 @@ module "blog_sg" {
   version = "4.13.0"
   name = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  /* vpc_id = data.aws_vpc.default.id */
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -52,11 +79,22 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+resource "aws_security_group_rule" "blog_http_in" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
-  tags = {
-    Name = "HelloWorld"
-  }
+    vpc_id = aws_security_group_blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+    vpc_id = aws_security_group_blog.id
 }
